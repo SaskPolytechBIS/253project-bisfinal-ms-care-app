1c29ebaf6bb988384f0355c91e565510469cf9d0
carretero3453 <carretero3453@saskpolytech.ca>
2024-05-31 13:33:36 -0600

    Major Revisions

--- MSCare Alpha/app/src/main/java/com/example/mscarealpha/ui/notes/AppointmentNotesFragment.java
+++ MSCare Alpha/app/src/main/java/com/example/mscarealpha/ui/notes/AppointmentNotesFragment.java
@@ -17,6 +17,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
 
 import com.example.mscarealpha.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -40,12 +42,32 @@ public class AppointmentNotesFragment extends Fragment {
         return new AppointmentNotesFragment();
     }
 
+    OvershootInterpolator interpolator = new OvershootInterpolator();
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notes, container, false);
         FloatingActionButton fabAdd_Notes = view.findViewById(R.id.fabAdd_Notes);
         FloatingActionButton fabDel_Notes = view.findViewById(R.id.fabDel_Notes);
+        FloatingActionButton fabOpenClose = view.findViewById(R.id.fab_open_close2);
+
+        TextView viewDel = view.findViewById(R.id.viewDel2);
+        TextView viewAdd = view.findViewById(R.id.viewAdd2);
+
+        Float translationYaxis = 100f;
+
+        final Boolean[] menuOpen = {false};
+
+        fabDel_Notes.setAlpha(0f);
+        fabAdd_Notes.setAlpha(0f);
+        viewDel.setAlpha(0f);
+        viewAdd.setAlpha(0f);
+
+        fabDel_Notes.setTranslationY(translationYaxis);
+        fabAdd_Notes.setTranslationY(translationYaxis);
+        fabDel_Notes.setTranslationY(translationYaxis);
+        fabAdd_Notes.setTranslationY(translationYaxis);
 
         fabAdd_Notes.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +108,42 @@ public class AppointmentNotesFragment extends Fragment {
 // set the adapter
 
         recyclerView.setAdapter(mAdapter);
+
+
+
+        fabOpenClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (menuOpen[0]){
+                    CloseMenu();
+                }
+                else{
+                    OpenMenu();
+                }
+            }
+
+            private void OpenMenu() {
+                menuOpen[0] = !menuOpen[0];
+                fabOpenClose.setImageResource(R.drawable.baseline_keyboard_arrow_down_24);
+                fabDel_Notes.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+                viewDel.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+                fabAdd_Notes.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+                viewAdd.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+//                fabReminders.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+//                viewReminders.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+            }
+
+            private void CloseMenu() {
+                menuOpen[0] = !menuOpen[0];
+                fabOpenClose.setImageResource(R.drawable.baseline_keyboard_arrow_up_24);
+                fabDel_Notes.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+                viewDel.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+                fabAdd_Notes.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+                viewAdd.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+//                fabReminders.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+//                viewReminders.animate().translationY(translationYaxis).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+            }
+        });
         return view;
 
 
